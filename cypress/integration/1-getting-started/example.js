87bfa0b9601a769608f6f4a92fff34f96c581cfd
kapurr <rajesh.kapur@volkerwessels.co.uk>
2022-06-15 15:29:54 +0100

    try 4ps

--- cypress/integration/1-getting-started/example.js
+++ cypress/integration/1-getting-started/example.js
@@ -4,24 +4,25 @@ describe('Getting to 4ps Website', function(){
 
        // cy.visit( "https://ce0550de.webitrent.com/ce0550de_web/wrd/run/etadm001gf.open");
 
-        //cy.visit("https://vwukdv1neweb01.volkerwessels.co.uk/BCS160Dev/Default",{
-        //auth: {
-       //  username: 'kapurr',
-      //  password: 'volker775544',
-      //   },
-         //failOnStatusCode: false
+        cy.visit("https://vwukdv1neweb01.volkerwessels.co.uk/BCS160Dev/",{
+        auth: {
+            username: '',
+            password: '',
+         },
+         failOnStatusCode: false
 
-        // })
+         })
+
        // cy.get('#username').type('volker/kapurr');
         //cy.get('#password').type('volker775544');
-        cy.visit("https://vwukts1webilb02.volkerwessels.co.uk/BCS160TEST/Default");
+      //  cy.visit("https://vwukts1webilb02.volkerwessels.co.uk/BCS160TEST/Default");
         //cy.visit('https://vwukdv1neweb01.volkerwessels.co.uk/BCS160Dev/Default/', {failOnStatusCode: false});      
         
         });
 
 
 
-
+        
 
 
 
